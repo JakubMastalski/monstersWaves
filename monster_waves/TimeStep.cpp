76d5ccb870d0dd02c2af5776f6525bc4d956a682
Jakub Mastalski <25287@student.ignatianum.edu.pl>
2024-10-30 11:36:21 +0100

    create TimeStep class

---
+++ monster_waves/TimeStep.cpp
@@ -0,0 +1,13 @@
+#include "TimeStep.hpp"
+
+TimeStep::TimeStep() : m_lastTime(m_clock.getElapsedTime())
+{
+    // Empty body
+}
+float TimeStep::getDeltaTime()
+{
+    const sf::Time currentTime = m_clock.getElapsedTime();
+    const sf::Time deltaTime = currentTime - m_lastTime;
+    m_lastTime = currentTime;
+    return deltaTime.asSeconds();
+}
