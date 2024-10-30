dbcd35d34281f831caf40dbcf93ffef1d85810a4
Jakub Mastalski <25287@student.ignatianum.edu.pl>
2024-10-30 11:27:28 +0100

    fill game class

--- main.cpp
+++ main.cpp
@@ -1,28 +1,9 @@
 #include <SFML/Graphics.hpp>
+#include "Game.hpp"
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "Przyk³ad SFML");
-
-    sf::RectangleShape rectangle(sf::Vector2f(200.f, 150.f));
-    rectangle.setFillColor(sf::Color::Red);
-    rectangle.setPosition(300.f, 225.f);
-
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-
-        window.clear(sf::Color::Black);
-
-        window.draw(rectangle);
-
-        window.display();
-    }
-
+    Game game;
+    game.run();
     return 0;
 }
--- monster_waves/Game.cpp
+++ monster_waves/Game.cpp
@@ -1,1 +1,38 @@
 #include "Game.hpp"
+#include "Game.hpp"
+
+Game::Game()
+{
+    // Empty constructor
+}
+void Game::run()
+{
+    while (!m_window.isDone())
+    {
+        handleEvents();
+        // update( 5.0f );
+        draw();
+    }
+}
+void Game::handleEvents()
+{
+    m_window.handleInput();
+}
+void Game::update(const float& dt)
+{
+    // Empty body.
+}
+void Game::draw()
+{
+    m_window.beginDraw();
+    sf::CircleShape circle(std::sin(m_stopwatch.getElapsedTime().asSeconds()) *
+        (static_cast<float>(m_window.getSize().x) / 8 +
+            static_cast<float>(m_window.getSize().y) / 4)
+    );
+    circle.setOrigin(sf::Vector2f(circle.getRadius(), circle.getRadius()));
+    circle.setPosition(static_cast<float>(m_window.getSize().x) * 0.5f,
+        static_cast<float>(m_window.getSize().y) * 0.5f);
+    circle.setFillColor(sf::Color::Yellow);
+    m_window.draw(circle);
+    m_window.endDraw();
+}
