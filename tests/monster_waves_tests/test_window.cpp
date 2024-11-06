0697357a7c3990961f6b789171e0e81fc2f86e39
Jakub Mastalski <25287@student.ignatianum.edu.pl>
2024-11-06 13:15:57 +0100

    add simple test for RandomGenerator class

--- tests/monster_waves_tests/test_window.cpp
+++ tests/monster_waves_tests/test_window.cpp
@@ -1,5 +1,5 @@
 #include <gtest/gtest.h>
-
+#include "RandomGenerator.hpp"
 #include "Window.hpp"
 
 class WindowTest : public ::testing::Test
@@ -12,3 +12,37 @@ protected:
 
     std::unique_ptr< Window > m_window;
 };
+
+class RandomNumberTest : public ::testing::Test
+{
+protected:
+    RandomNumber randGen{ 1, 10 };
+};
+
+TEST_F(RandomNumberTest, TestInRange)
+{
+    int number = randGen.getNumber();
+
+    EXPECT_GE(number, 1);
+    EXPECT_LE(number, 10);
+}
+
+TEST_F(RandomNumberTest, TestMinMaxOrder)
+{
+    int number = randGen.getNumber();
+
+    EXPECT_GE(number, 1);
+    EXPECT_LE(number, 10);
+}
+
+TEST_F(RandomNumberTest, ExpectExactResult)
+{
+    RandomNumber randomGenEE(5, 5);
+
+    int number = randomGenEE.getNumber();
+
+    EXPECT_EQ(number, 5);
+}
+
+
+
