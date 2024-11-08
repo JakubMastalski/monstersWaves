fc9d05c798c8a4273787794cbcdade91f8206d43
Jakub Mastalski <25287@student.ignatianum.edu.pl>
2024-11-08 14:06:34 +0100

    small main changes

--- main.cpp
+++ main.cpp
@@ -7,5 +7,19 @@ int main()
 {
     Game game;
     game.run();
+
+    RandomNumber r1(1, 6);
+    unsigned int number1 = r1.getNumber();
+    std::cout << number1;
+    RandomNumber r2(1, 6);
+    unsigned int number2 = r2.getNumber();
+    std::cout << number1;
+    RandomNumber r3(1, 6);
+    unsigned int number3 = r3.getNumber();
+    std::cout << number3;
+    RandomNumber r4(1, 6);
+    unsigned int number4 = r4.getNumber();
+    std::cout << number4;
+
     return 0;
 }
