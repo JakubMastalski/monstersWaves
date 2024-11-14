2a5cb17346d5ad4b26e1d342981a2f4fd295ad19
Jakub Mastalski <25287@student.ignatianum.edu.pl>
2024-11-14 12:29:55 +0100

    move template in RandomGenerator.cpp to .hpp file

--- monster_waves/RandomGenerator.cpp
+++ monster_waves/RandomGenerator.cpp
@@ -5,18 +5,3 @@ RandomNumber::RandomNumber() : mt(std::random_device{}())
    //EMPTY BODY
 }
 
-template<typename T>
-
-T RandomNumber::getNumber(T minNumber,T maxNumber)
-{
-    if constexpr (std::is_integral<T>::value)
-    {
-        std::uniform_int_distribution<T> dist(minNumber, maxNumber);
-        return dist(mt);
-    }
-    else if constexpr(std::is_floating_point<T>::value)
-    {
-        std::uniform_real_distribution<T> dist(minNumber, maxNumber);
-        return dist(mt);
-    }
-}
