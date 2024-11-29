f903a84db9cbdb3a68e5e24bcb441508ae7e42b7
Jakub Mastalski <25287@student.ignatianum.edu.pl>
2024-11-29 21:58:47 +0100

    improve splash screen , init window object in init method insted in constructor, add font and text initzialisation

--- MonsterWaves/Screen/SplashScreen.cpp
+++ MonsterWaves/Screen/SplashScreen.cpp
@@ -1,16 +1,16 @@
 #include "SplashScreen.hpp"
 
-SplashScreen::SplashScreen(Window window, float duration)
-	: m_window(std::move(window)), m_windowDuration(duration)
+SplashScreen::SplashScreen(float duration)
+	: m_windowDuration(duration)
 {
-	m_window.setup({ 800, 600 }, "Splash Screen");
+	initWindow();
 }
 
 void SplashScreen::update()
 {
 	float deltaTime = m_timeStep.getDeltaTime();
 
-	if (m_timeStep.getTotalTimeInSeconds() >= windowDuration)
+	if (m_timeStep.getTotalTimeInSeconds() >= m_windowDuration)
 	{
 		m_window.close();
 	}
@@ -29,7 +29,7 @@ void SplashScreen::render()
 void SplashScreen::draw()
 {
 	m_window.beginDraw();
-	m_window.draw()
+	m_window.draw(m_text);
 	m_window.endDraw();
 }
 
@@ -40,5 +40,16 @@ void SplashScreen::handleEvent()
 
 void SplashScreen::initWindow()
 {
+	m_window.setup({ 1000, 800 }, "Splash Screen");
 
+	if (!font.loadFromFile("bebasFont.ttf")) {
+		std::cerr << "The font could not be loaded!" << std::endl;
+		return;
+	}
+
+	text.setFont(font);
+	text.setString("Siema!"); 
+	text.setCharacterSize(30); 
+	text.setFillColor(sf::Color::White);
+	text.setPosition(200, 300);
 }
