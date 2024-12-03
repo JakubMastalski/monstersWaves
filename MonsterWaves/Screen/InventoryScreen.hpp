8669f094c157b76c3ac503a0553dee17531f2fbb
Jakub Mastalski <25287@student.ignatianum.edu.pl>
2024-12-03 16:49:02 +0100

    add full InventoryScreen class

---
+++ MonsterWaves/Screen/InventoryScreen.hpp
@@ -0,0 +1,17 @@
+#pragma once
+#include "Screen/BaseScreen.hpp"
+
+class InventoryScreen final : public BaseScreen
+{
+public:
+    explicit InventoryScreen(Window& window);
+
+public:
+    void handleEvents() override;
+    void update(float dt) override;
+    void render() override;
+
+private:
+    std::vector< sf::RectangleShape >   m_squares;
+    std::vector< sf::CircleShape >      m_circles;
+};
