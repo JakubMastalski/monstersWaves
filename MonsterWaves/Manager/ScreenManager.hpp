abea3bba934b30358cab77ae95aab182f888ea30
Jakub Mastalski <25287@student.ignatianum.edu.pl>
2024-12-16 13:06:45 +0100

    implement ScreenManager class

--- MonsterWaves/Manager/ScreenManager.hpp
+++ MonsterWaves/Manager/ScreenManager.hpp
@@ -1,6 +1,40 @@
 #pragma once
 
+#include "Screen/BaseScreen.hpp"
+#include "Screen/ScreenType.hpp"
+#include <unordered_map>
+
 class ScreenManager
 {
+public:
+    using ScreenPointer = std::unique_ptr< BaseScreen >;
 
+public:
+    ScreenManager(const ScreenManager&) = delete;
+    ScreenManager& operator=(const ScreenManager&) = delete;
+
+public:
+    static void Init();
+    static void ShutDown();
+    static ScreenManager& GetInstance();
+
+private:
+    ScreenManager();
+    ~ScreenManager() = default;
+
+public:
+    void setScreen(ScreenType screenType);
+
+public:
+    bool isRunning() const
+    {
+        return m_activeScreen->isRunning();
+    }
+    void handleEvents() const;
+    void update(float dt) const;
+    void render() const;
+
+private:
+    std::unordered_map< ScreenType, ScreenPointer >     m_screens;
+    BaseScreen* m_activeScreen{ nullptr };
 };
