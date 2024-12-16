446c91d599c4696a22e1ee358ded98e62a3cd23c
Jakub Mastalski <25287@student.ignatianum.edu.pl>
2024-12-16 12:43:47 +0100

    refactor BaseScreen class to resolve events procesing

--- MonsterWaves/Screen/BaseScreen.cpp
+++ MonsterWaves/Screen/BaseScreen.cpp
@@ -4,10 +4,7 @@ BaseScreen::BaseScreen(Window* window) : m_window(window)
 {
     // Empty body.
 }
-void BaseScreen::handleEvents()
-{
-    m_window->handleEvents();
-}
+
 bool BaseScreen::isRunning() const
 {
     return m_window->isRunning();
