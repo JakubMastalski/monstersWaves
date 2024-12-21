e1fc447d6e5db27471229ec984ee30012ebf00dc
Jakub Mastalski <25287@student.ignatianum.edu.pl>
2024-12-21 17:42:20 +0100

    implement basic enemy class following player

--- MonsterWaves/Entity/Enemy.cpp
+++ MonsterWaves/Entity/Enemy.cpp
@@ -1,5 +1,4 @@
 #include "Enemy.hpp"
-
 #include "Utils/RandomGenerator.hpp"
 
 Enemy::Enemy(const Window* window, const sf::Vector2f& playerPosition, const float speed)
@@ -19,45 +18,55 @@ Enemy::Enemy(const Window* window, const sf::Vector2f& playerPosition, const flo
     const float length = std::sqrt(direction.x * direction.x + direction.y * direction.y);
     m_direction = direction / length;
 
-    m_circle.setRadius(20.0f);
-    m_circle.setOrigin(m_circle.getRadius(), m_circle.getRadius());
-    m_circle.setPosition(m_position);
-    m_circle.setFillColor(sf::Color::Red);
+    m_movingTexture.loadFromFile("res/images/EnemyRun.png");
+    m_sprite.setTexture(m_movingTexture);
+
+
+    m_sprite.setPosition(m_position);
+    m_sprite.setOrigin(20.0f, 20.0f);
+
+    for (int i = 0; i < m_movingRects.size(); ++i)
+    {
+        m_movingRects[i] = sf::IntRect{ i * 80, 0, 80, 80 };
+    }
+
+    m_sprite.setTextureRect(m_movingRects[m_currentFrame]);
+    m_sprite.setScale(1.5f, 1.5f);
 }
 
 void Enemy::update(const float dt, const sf::Vector2f& playerPosition)
 {
-    m_position += m_direction * m_speed * dt;
+    updateMove(dt, playerPosition);
+    updateMoveAnimation();
+}
 
-    const sf::Vector2f newDirection = playerPosition - m_position;
-    const float length = std::sqrt(newDirection.x * newDirection.x + newDirection.y * newDirection.y);
+void Enemy::updateMove(const float dt, const sf::Vector2f& playerPosition)
+{
+    const sf::Vector2f direction = playerPosition - m_position;
+    const float length = std::sqrt(direction.x * direction.x + direction.y * direction.y);
     if (length != 0)
     {
-        m_direction = newDirection / length;
+        m_direction = direction / length;
     }
+
+    m_position += m_direction * m_speed * dt;
+    m_sprite.setPosition(m_position);
 
     m_animationTimer += dt;
+
     if (m_animationTimer >= 0.07f)
     {
-        m_animationTimer = 0.0f;
+        m_animationTimer = 0.40f;
 
-        float radius = m_circle.getRadius();
-        if (m_animationDirection > 0 && radius >= 22.0f)
-        {
-            m_animationDirection = -1;
-        }
-        else if (m_animationDirection < 0 && radius <= 18.0f)
-        {
-            m_animationDirection = 1;
-        }
-        m_circle.setRadius(radius + static_cast<float>(m_animationDirection));
-        m_circle.setOrigin(m_circle.getRadius(), m_circle.getRadius());
+        m_sprite.setTexture(m_movingTexture);
     }
-
-    m_circle.setPosition(m_position);
+}
+void Enemy::updateMoveAnimation()
+{
+    
 }
 
 void Enemy::draw(Window* window) const
 {
-    window->draw(m_circle);
+    window->draw(m_sprite);
 }
--- MonsterWaves/Entity/Enemy.hpp
+++ MonsterWaves/Entity/Enemy.hpp
@@ -2,6 +2,8 @@
 
 #include <Screen/Window.hpp>
 
+#include <array>
+
 class Enemy
 {
 public:
@@ -12,12 +14,32 @@ public:
     void draw(Window* window) const;
 
 private:
-    sf::CircleShape m_circle;
-    sf::Vector2f  m_position;
-    sf::Vector2f m_direction;
+    void updateAnimation(float dt);
 
+private:
+    sf::CircleShape m_circle;
+    sf::Vector2f m_position;
+    sf::Vector2f m_direction;
+private:
     float m_speed;
 
     float m_animationTimer{ 0.0f };
     int   m_animationDirection{ 1 };
+
+    float m_animationMoveIdleTime{ 0.0f };
+   const float m_frameDuration{ 0.07f };
+
+private:
+    sf::Texture m_movingTexture;
+    sf::Texture m_idleTexture;
+    sf::Sprite  m_sprite;
+private:
+    std::array<sf::IntRect, 9> m_movingRects;
+    int  m_currentFrame{ 0 };
+private:
+    float m_frameTime{ 0.07f };
+
+private:
+    void updateMoveAnimation();
+    void updateMove(const float dt, const sf::Vector2f& playerPosition);
 };
