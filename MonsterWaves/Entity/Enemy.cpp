9200458c38c37480ae363bf599acbc8af0a71b15
Jakub Mastalski <25287@student.ignatianum.edu.pl>
2024-12-22 19:08:33 +0100

    create base enemy dead animation

--- MonsterWaves/Entity/Enemy.cpp
+++ MonsterWaves/Entity/Enemy.cpp
@@ -19,6 +19,8 @@ Enemy::Enemy(const Window* window, const sf::Vector2f& playerPosition, const flo
     m_direction = direction / length;
 
     m_movingTexture.loadFromFile("res/images/EnemyRun.png");
+    m_deadTexture.loadFromFile("res/images/EnemyDead.png");
+
     m_sprite.setTexture(m_movingTexture);
 
 
@@ -30,30 +32,80 @@ Enemy::Enemy(const Window* window, const sf::Vector2f& playerPosition, const flo
         m_movingRects[i] = sf::IntRect{ i * 80, 0, 80, 80 };
     }
 
+    for (int i = 0; i < m_deadRects.size(); ++i)
+    {
+        m_deadRects[i] = sf::IntRect{ i * 80, 0, 80, 80 };
+    }
+
     m_sprite.setTextureRect(m_movingRects[m_currentFrame]);
     m_sprite.setScale(1.5f, 1.5f);
 }
 
 void Enemy::update(const float dt, const sf::Vector2f& playerPosition)
 {
-    updateMove(dt, playerPosition);
-    updateMoveAnimation(dt);
+    if (enemyDead)
+    {
+        updateDeadAnimation(dt); 
+    }
+    else
+    {
+        updateMove(dt, playerPosition);
+        updateMoveAnimation(dt);
+    }
 }
 
 void Enemy::updateMove(const float dt, const sf::Vector2f& playerPosition)
 {
-        const sf::Vector2f direction = playerPosition - m_position;
+    const sf::Vector2f direction = playerPosition - m_position;
     const float length = std::sqrt(direction.x * direction.x + direction.y * direction.y);
-    if (length != 0)
+
+    if (length < 1.0f) 
+    {
+        enemyDead = true; 
+        m_currentFrame = 0; 
+        m_sprite.setTexture(m_deadTexture);
+        return;
+    }
+    
+    if (!enemyDead)
     {
         m_direction = direction / length;
+        m_position += m_direction * m_speed * dt;
+        m_sprite.setPosition(m_position);
     }
-
-    m_position += m_direction * m_speed * dt;
-
-    m_sprite.setPosition(m_position);
 }
 void Enemy::updateMoveAnimation(const float dt)
+{
+    if (!enemyDead)
+    {
+        m_animationTimer += dt;
+
+        if (m_animationTimer >= m_frameDuration)
+        {
+            m_animationTimer = 0.0f;
+            ++m_currentFrame;
+
+            if (m_currentFrame >= m_movingRects.size())
+            {
+                m_currentFrame = 0;
+            }
+            m_sprite.setTextureRect(m_movingRects[m_currentFrame]);
+        }
+
+        if (m_direction.x > 0)
+        {
+            m_sprite.setScale(1.5f, 1.5f);
+            m_sprite.setOrigin(0.0f, 0.0f);
+        }
+        else if (m_direction.x < 0)
+        {
+            m_sprite.setScale(-1.5f, 1.5f);
+            m_sprite.setOrigin(80.0f, 0.0f);
+        }
+    }
+}
+
+void Enemy::updateDeadAnimation(const float dt)
 {
     m_animationTimer += dt;
 
@@ -62,22 +114,11 @@ void Enemy::updateMoveAnimation(const float dt)
         m_animationTimer = 0.0f;
         ++m_currentFrame;
 
-        if (m_currentFrame >= m_movingRects.size())
+        if (m_currentFrame >= m_deadRects.size())
         {
-            m_currentFrame = 0;
+            m_currentFrame = m_deadRects.size() - 1;
         }
-        m_sprite.setTextureRect(m_movingRects[m_currentFrame]);
-    }
-
-    if (m_direction.x > 0) 
-    {
-        m_sprite.setScale(1.5f, 1.5f); 
-        m_sprite.setOrigin(0.0f, 0.0f); 
-    }
-    else if (m_direction.x < 0) 
-    {
-        m_sprite.setScale(-1.5f, 1.5f); 
-        m_sprite.setOrigin(80.0f, 0.0f);
+        m_sprite.setTextureRect(m_deadRects[m_currentFrame]);
     }
 }
 
--- MonsterWaves/Entity/Enemy.hpp
+++ MonsterWaves/Entity/Enemy.hpp
@@ -23,20 +23,23 @@ private:
     float m_animationTimer{ 0.0f };
     int   m_animationDirection{ 1 };
 
-    float m_animationMoveIdleTime{ 0.0f };
+   float m_animationMoveIdleTime{ 0.0f };
    const float m_frameDuration{ 0.07f };
-
+   bool enemyDead = false;
 private:
     sf::Texture m_movingTexture;
-    sf::Texture m_idleTexture;
+    sf::Texture m_deadTexture;
     sf::Sprite  m_sprite;
 private:
     std::array<sf::IntRect, 5> m_movingRects;
-    int  m_currentFrame{ 0 };
-private:
-    float m_frameTime{ 0.07f };
+    std::array<sf::IntRect, 17> m_deadRects;
 
 private:
+    float m_frameTime{ 0.07f };
+    int  m_currentFrame{ 0 };
+private:
     void updateMoveAnimation(const float dt);
+    void updateDeadAnimation(const float dt);
+
     void updateMove(const float dt, const sf::Vector2f& playerPosition);
 };
