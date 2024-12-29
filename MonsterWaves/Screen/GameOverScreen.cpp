709801fdb6febbdb1b2a74b313ea213ae2481a32
Jakub Mastalski <25287@student.ignatianum.edu.pl>
2024-12-29 23:37:26 +0100

    correct spelling on gameover screen

--- MonsterWaves/Screen/GameOverScreen.cpp
+++ MonsterWaves/Screen/GameOverScreen.cpp
@@ -9,7 +9,7 @@ GameOverScreen::GameOverScreen(Window* window) : BaseScreen(window)
 
     m_font.loadFromFile("res/fonts/aleo/Aleo-Italic.otf");
     m_text.setFont(m_font);
-    m_text.setString("Gave Over!");
+    m_text.setString("Game Over!");
     m_text.setCharacterSize(32);
     m_text.setFillColor(sf::Color::White);
     m_text.setPosition(static_cast<float>(m_window->getSize().x * 0.5 - m_text.getGlobalBounds().width * 0.5),
